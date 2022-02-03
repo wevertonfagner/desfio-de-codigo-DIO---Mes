82bdd98a2cc07a2c1a351a7d895e8a7b6850bb9b
Weverton Fagner <91743237+wevertonfagner@users.noreply.github.com>
2022-02-03 20:48:44 -0300

    Update Program.cs

--- desfio de codigo DIO - Mes/Program.cs
+++ desfio de codigo DIO - Mes/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+Entrada
+A entrada contém um único valor inteiro.
+
+Saída
+Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
